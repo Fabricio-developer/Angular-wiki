f8eda29dd279f71b3fee9a9cc625eee0644a627e
Fabrício Santos <120188885+Fabricio-developer@users.noreply.github.com>
2024-11-18 12:12:49 -0300

    Feat: create a typed form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TypedFormsComponent } from './components/forms/typed-forms/typed-forms.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TypedFormsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/forms/typed-forms/typed-forms.component.ts
+++ src/app/components/forms/typed-forms/typed-forms.component.ts
@@ -1,25 +1,30 @@
+import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-typed-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './typed-forms.component.html',
   styleUrl: './typed-forms.component.scss'
 })
 export class TypedFormsComponent {
 
-  private formBuilderService = inject(FormBuilder);
+  // Se caso nenhum campo pode ser null, utilize a instância do formBuild er que é feita para isso
+  private formBuilderService = inject(NonNullableFormBuilder);
 
   protected form = this.formBuilderService.group({
-    name: ['', { validators: [] }],
-    email: ['', { validators: [] }],
+    name: ['', { validators: [Validators.required] }],
+    email: ['', { validators: [], nonNullable: true }],
     pin: [0, { validators: [] }]
-  })
+  },)
 
   protected printForm(){
     console.log(this.form.value);
+
+    this.form.reset()
+    console.log("Form reseted! ",this.form.value);
 
   }
 
